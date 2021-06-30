614f54e106547997f34da2523621636348337d67
diyalizbethjose <84697287+diyalizbethjose@users.noreply.github.com>
2021-06-30 22:25:30 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,67 +1,84 @@
+
+
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <cs50.h>
+#include <ctype.h>
 
-string ari (string s)
-{
-int words=0,sentence=0,character=0;
-int ch;
-for(int i=0;s[i]!='\0';i++)
-{
-if(s[i]=='\n'||s[i]=='?'||s[i]=='!')
-{
-sentence++;
-words++;
-}
-else 
-{
-if(s[i]==' '||s[i]=='\t')
-{
-words++;
-character++;
-}
-else
-character++;
-}
 
-}
-ch= (4.71*(character/words)) + (.5*(words/sentence)) - 21.43;
-for(int j=0;j++)
-{
-if(ch>=j&&ch<(j+1))
-{
-ch=(j+1);
-break;
-}
-}
-if(ch==1)
-return("Kindergarten");
-else if(ch==2)
-return("First/Second Grade");
-else if(ch==3)
-return("Third Grade");
-else if(ch==4)
-return("Fourth Grade");
-else if(ch==5)
-return("Fifth Grade");
-else if(ch==6)
-return("Sixth Grade");
-else if(ch==7)
-return("Seventh Grade");
-else if(ch==8)
-return("Eighth Grade");
-else if(ch==9)
-return("Ninth Grade");
-else if(ch==10)
-return("Tenth Grade");
-else if(ch==11)
-return("Eleventh Grade");
-else if(ch==12)
-return("Twelfth grade");
-else if(ch==13)
-return("College student");
-else if(ch==14)
-return("Professor");
-}
 
+string ari(string s)
+{
+float n=strlen(s);
+                                                                    /*printf("no of elements is %f\n",n);*/
+
+    float c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(isalnum(s[i])!=0)
+        {
+            c=c+1;
+        }
+    }
+
+                                                                    /* printf("mo of characters is %f",c);*/
+    float a=0;
+    for(int i=0;i<n;i++)
+    {
+        if (s[i]==' ')
+        {
+            a=a+1;
+        }
+    }
+                                                                     /*printf("no of words is %f\n",a);*/
+    float b=0;
+    for(int i=0;i<n;i++)
+    {
+        if(s[i]=='?'||s[i]=='.')
+        {
+            b=b+1;
+        }
+        else if(s[i]=='!')
+        {
+            b=b+1;
+        }
+    }
+                                                                   /* printf("no of sentences is %f\n",b);*/
+
+
+float k;
+k=(4.71*c/a)+(0.5*a/b)-21.43;
+                                                                    /*printf("k is %f\n",k);*/
+
+string x;
+if(k>13)
+{x="Professor";}
+if(k>12&&k<=13)
+{x="College student";}
+if(k>11&&k<=12)
+{x="Twelfth grade";}
+if(k>10&&k<=11)
+{x="Eleventh Grade";}
+if(k>9&&k<=10)
+{x="Tenth Grade";}
+if(k>8&&k<=9)
+{x="Ninth Grade";}
+if(k>7&&k<=8)
+{x="Eighth Grade";}
+if(k>6&&k<=7)
+{x="Seventh Grade";}
+if(k>5&&k<=6)
+{x="Sixth Grade";}
+if(k>4&&k<=5)
+{x="Fifth Grade";}
+if(k>3&&k<=4)
+{x="Fourth Grade";}
+if(k>2&&k<=3)
+{x="Third Grade";}
+if(k>1&&k<=2)
+{x="First/Second Grade";}
+if(k>0&&k<=1)
+{x="Kindergarten";}
+return x;
+}
